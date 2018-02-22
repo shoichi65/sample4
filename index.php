2fba41c3ea6d4f442fbf72057a80f5956c3de6cb
shoichi65 <shoichi65@gmail.com>
2018-02-22 14:08:43 +0800

    [WIP] index生成

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
     echo 'Hello world';
     var_dump(1);
+
+    $name = 'Shoichi Chikushi';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <h1>Hello Github!</h1>
+</body>
+</html>
